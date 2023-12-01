94ec6e5a9906d805f26effdae8d719c09752b670
__Pug__ <59162541+Pugpaprika21@users.noreply.github.com>
2023-12-01 14:14:08 +0700

    Update BaseController.php

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -4,3 +4,14 @@ class BaseController extends R
 {
     //
 }
+
+// class BaseController
+// {
+//     /** ORM Redbeen */
+//     protected R $rb;
+    
+//     public function __construct() 
+//     {
+//         $this->rb = new R();
+//     }
+// }
